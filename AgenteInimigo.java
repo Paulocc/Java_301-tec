049f326c2b654f3c8cbda0a3d5f3c4c71d61eaf6
Paulo Sergio Nunes <56320754+Paulocc@users.noreply.github.com>
2026-09-02 08:21:00 -0300

    Add files via upload

---
+++ AgenteInimigo.java
@@ -0,0 +1,107 @@
+/**
+ * Um exemplo de agente que anda aleatoriamente na arena. Esse agente pode ser usado como base
+ * para a criação de um agente mais esperto. Para mais explicações, veja o AgenteDummy.java.
+ * 
+ * Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
+ */
+
+package br.uffs.cc.jarena;
+
+public class AgenteInimigo extends Agente
+{
+	public AgenteInimigo(Integer x, Integer y, Integer energia) {
+		super(x, y, energia);
+		setDirecao(geraDirecaoAleatoria());
+	}
+	int guardaBaixo=0, guardaCima=0, guardaDireita=0, guardaEsquerda=0;
+	boolean ganhaEnergia = false;
+
+	public void pensa() {
+		// Se não conseguimos nos mover para a direção atual, quer dizer
+		// que chegamos no final do mapa ou existe algo bloqueando nosso
+		// caminho.
+		if(!podeMoverPara(getDirecao())) {
+			// Como não conseguimos nos mover, vamos escolher uma direção
+			// nova.
+			setDirecao(geraDirecaoAleatoria());
+		}
+		
+		// Se o agente conseguie se dividir (tem energia) e se o total de energia
+		// do agente é maior que 1100, nos dividimos. O agente filho terá a metade
+		// da nossa energia atual.
+		if(podeDividir() && getEnergia() >= 1100) {
+			divide();
+			setDirecao(geraDirecaoAleatoria());
+		}
+
+		if(getEnergia()<=150){
+			//se tem 150 de energia para() é executado para o agente sobreviver mais tempo.
+			para();
+		}
+
+		if(isParado() && getEnergia() > 150 && ganhaEnergia == false){
+			//se o agente parou, mas está co mais de 150 de energia ele volta a andar.
+			setDirecao(geraDirecaoAleatoria());
+		}
+		if(getDirecao()==Agente.ESQUERDA){
+			guardaEsquerda+=1;
+		}
+		if(getDirecao()==Agente.DIREITA){
+			guardaDireita+=1;
+		}
+		if(getDirecao()==Agente.CIMA){
+			guardaCima+=1;
+		}
+		if(getDirecao()==Agente.BAIXO){
+			guardaBaixo+=1;
+		}
+		if(guardaBaixo==25 || guardaCima==25 || guardaDireita==25 || guardaEsquerda==25){
+			guardaBaixo=0;
+			guardaCima=0;
+			guardaDireita=0;
+			guardaEsquerda=0;
+			setDirecao(geraDirecaoAleatoria());
+		}
+	}
+
+	public void moverPara(int x, int y) {
+		//Faz os agentes irem a determinada coordenada.
+		if (getX() < x) {
+			setDirecao(DIREITA);
+		} else if (getX() > x) {
+			setDirecao(ESQUERDA);
+		} else if (getY() > y) {
+			setDirecao(CIMA);
+		} else if (getY() < y) {
+			setDirecao(BAIXO);
+		}
+	}
+	
+	public void recebeuEnergia() {
+		ganhaEnergia = true;
+		para();
+		String guardaX = Integer.toString(getX());
+		String guardaY = Integer.toString(getY());
+		enviaMensagem(guardaX + "," + guardaY);
+	}
+	public void tomouDano(int energiaRestanteInimigo) {
+		setDirecao(geraDirecaoAleatoria());
+	}
+	public void ganhouCombate() {
+		setDirecao(geraDirecaoAleatoria());
+	}
+	public void recebeuMensagem(String msg) {
+		//Recebe as coordenadas de um agente e chama o moverPara(coordenadaX, coordenadaY)
+		String[] coordenadas = msg.split(",");
+		int coordenadaX = Integer.parseInt(coordenadas[0]);
+		int coordenadaY = Integer.parseInt(coordenadas[1]);
+		moverPara(coordenadaX, coordenadaY);
+		
+		//Invocado sempre que um agente aliado próximo envia uma mensagem.
+	}
+	
+	public String getEquipe() {
+		// Definimos que o nome da equipe do agente é "Inimigo".
+		return "Inimigo";
+	}
+}
